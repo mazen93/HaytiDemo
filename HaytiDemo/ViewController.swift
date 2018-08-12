d7fe31966c3ae6fdf12a3565fcae20c0f8b86fe3
Mohamed <elfishawy.m@yahoo.com>
2018-08-12 14:12:18 +0200

    Initial Commit

---
+++ HaytiDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HaytiDemo
+//
+//  Created by tr on 8/12/18.
+//  Copyright © 2018 tr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
